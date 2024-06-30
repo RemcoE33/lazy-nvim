48f7b22a25ced0dec077df2a68621e0aeb3410bf
Remco Edelenbos <remco.edelenbos@gmail.com>
2024-06-30 12:23:32 +0200

    update

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,8 +1,36 @@
-local cmp = require("cmp")
 return {
-  "hrsh7th/nvim-cmp",
-  keys = {
-    ["<CR>"] = cmp.mapping.confirm({ select = false }),
-    ["<TAB>"] = cmp.mapping.confirm({ select = true }),
+  {
+    "hrsh7th/nvim-cmp",
+    -- event = "InsertEnter",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "saadparwaiz1/cmp_luasnip",
+    },
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      local cmp = require("cmp")
+
+      opts.preselect = cmp.PreselectMode.None
+      opts.completion = {
+        completeopt = "menu,menuone,noinsert,noselect",
+      }
+      opts.mapping = vim.tbl_extend("force", opts.mapping, {
+        -- My config
+        -- ["<Down>"] = cmp.config.disable,
+        -- ["<Up>"] = cmp.config.disable,
+        ["<CR>"] = cmp.mapping.confirm({ select = false }), -- 'select = false' to only confirm explicitly selected item
+        ["<TAB>"] = cmp.mapping.confirm({ select = false, behavior = cmp.ConfirmBehavior.Replace }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+
+        -- LazyVim defaults
+        ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+        ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.abort(),
+      })
+    end,
   },
 }
